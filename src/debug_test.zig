9109d2866f0cffbc3847322b474871679749112b
Eric San <ericsssan@gmail.com>
2026-06-10 18:45:01 +0800

    debug_test: assignment-narrowing repro scratch

--- src/debug_test.zig
+++ src/debug_test.zig
@@ -3,13 +3,16 @@ const parser = @import("es_parser");
 const Checker = @import("ez_checker").Checker;
 const NodeIndex = parser.ast.NodeIndex;
 
-test "setter rest param" {
+test "assignment narrowing reads" {
     const gpa = std.testing.allocator;
     const src =
-        \\class C {
-        \\    set X(...v) { }
-        \\}
-        \\function f(...args) { return args; }
+        \\var x = true;
+        \\var a;
+        \\a = x;
+        \\var y = 5;
+        \\var b = y;
+        \\x = false;
+        \\var c = x;
     ;
     var lex = try parser.Lexer.tokenizeWithLanguage(gpa, src, .ts);
     defer lex.deinit(gpa);
@@ -27,22 +30,14 @@ test "setter rest param" {
     var n: u32 = 1;
     while (n < ast.nodes.len) : (n += 1) {
         const ni: NodeIndex = @enumFromInt(n);
+        if (ast.nodeTag(ni) != .identifier) continue;
         const span = ast.nodeSpan(ni);
         if (span.end > src.len or span.end <= span.start) continue;
         const text = src[span.start..span.end];
-        if (!std.mem.eql(u8, text, "v") and !std.mem.eql(u8, text, "args") and !std.mem.eql(u8, text, "...v")) continue;
-        const parents = sem.parent_indices;
-        const pidx = parents[n];
-        var ptag: []const u8 = "none";
-        var gtag: []const u8 = "none";
-        if (pidx != @intFromEnum(NodeIndex.none)) {
-            ptag = @tagName(ast.nodeTag(@enumFromInt(pidx)));
-            const gidx = parents[pidx];
-            if (gidx != @intFromEnum(NodeIndex.none)) gtag = @tagName(ast.nodeTag(@enumFromInt(gidx)));
-        }
+        const line = std.mem.count(u8, src[0..span.start], "\n");
         const ty = checker.typeOf(ni);
         const tystr = try checker.typeToString(ty);
         defer gpa.free(tystr);
-        std.debug.print("node {d} tag={s} parent={s} gp={s} text='{s}' ty='{s}'\n", .{ n, @tagName(ast.nodeTag(ni)), ptag, gtag, text, tystr });
+        std.debug.print("L{d} '{s}' = {s}\n", .{ line, text, tystr });
     }
 }
